c7f590747ad9ea483c0c9351a77485a0049a8997
kblandonv <kblandonv@unal.edu.co>
2024-04-08 10:45:27 -0500

    Add router and register views

--- Proyectos/Test_Triduum-main/ecommerceapi/product_api/routers.py
+++ Proyectos/Test_Triduum-main/ecommerceapi/product_api/routers.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from views import ProductViewSet, OrderViewSet, OrderDetailViewSet
 
-
+# Creamos un enrutador para nuestras vistas
 router = DefaultRouter()
+
+# Registramos las vistas con el enrutador
 router.register(r'products', ProductViewSet, basename='products')
 router.register(r'orders', OrderViewSet, basename='orders')
 router.register(r'orderdetails', OrderDetailViewSet, basename='orderdetails')
-router.register(r'create', OrderDetailViewSet, basename='create')
