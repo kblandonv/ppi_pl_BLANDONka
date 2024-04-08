6be4bc41ae20214c32d20244a171456d2de51462
kblandonv <kblandonv@unal.edu.co>
2024-04-08 10:46:01 -0500

    Update URLs in product_api module

--- Proyectos/Test_Triduum-main/ecommerceapi/product_api/urls.py
+++ Proyectos/Test_Triduum-main/ecommerceapi/product_api/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 from product_api.views import ProductViewSet, OrderViewSet, OrderDetailViewSet
 
-# Create a router and register our viewsets with it.
+# Creamos un enrutador y registramos nuestros viewsets con él.
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet, basename='products')
 router.register(r'orders', OrderViewSet, basename='orders')
 router.register(r'orderdetails', OrderDetailViewSet, basename='orderdetails')
-router.register(r'create', OrderDetailViewSet, basename='create')
 
+# Definimos las URL de nuestra API
 urlpatterns = [
     path('', include(router.urls)),
 ]
